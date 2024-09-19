7a051c3f0ee096e7b3ca7dcdbf13348439ace46f
yaaarsl_v <y.savenkov@crafttech.ru>
2024-09-19 15:27:44 +0300

    refactor

--- src/main/resources/db.migrations/V1__Create_Drink_And_Stats.sql
+++ src/main/resources/db.migrations/V1__Create_Drink_And_Stats.sql
@@ -1,13 +1,15 @@
-CREATE TABLE drink (
-                       id BIGSERIAL PRIMARY KEY,
-                       name VARCHAR(255) NOT NULL,
-                       water_amount INT NOT NULL,
-                       coffee_amount INT NOT NULL,
-                       milk_amount INT NOT NULL
+CREATE TABLE drink
+(
+    id            BIGSERIAL PRIMARY KEY,
+    name          VARCHAR(255) NOT NULL,
+    water_amount  INT          NOT NULL,
+    coffee_amount INT          NOT NULL,
+    milk_amount   INT          NOT NULL
 );
 
-CREATE TABLE drink_stats (
-                             id BIGSERIAL PRIMARY KEY,
-                             drink_id BIGINT REFERENCES drink(id),
-                             times_ordered INT NOT NULL
+CREATE TABLE drink_stats
+(
+    id            BIGSERIAL PRIMARY KEY,
+    drink_id      BIGINT REFERENCES drink (id),
+    times_ordered INT NOT NULL
 );
